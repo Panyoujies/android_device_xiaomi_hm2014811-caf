f599107eee9c5db21cd9cc8a9270d99c012057ce
Silentlys <1561707299@qq.com>
2015-06-13 21:45:57 +0800

    update lollipop

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,15 +21,10 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#define BTM_DEF_LOCAL_NAME   "xiaomi"
-#define BLUETOOTH_QTI_SW TRUE
+#define BTM_DEF_LOCAL_NAME   "HM2014811"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_VND_INCLUDED   TRUE
-#define BLE_PERIPHERAL_ADV_NAME  TRUE
 #endif
